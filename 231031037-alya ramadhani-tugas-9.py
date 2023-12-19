a89d8024043e2535b0b81da5ee666c5664efc9e9
alyaramadhanii <153715494+alyaramadhanii@users.noreply.github.com>
2023-12-19 21:18:13 +0800

    Add files via upload

---
+++ 231031037-alya ramadhani-tugas-9.py
@@ -0,0 +1,18 @@
+biodata =         {'nama'               : 'Alya ramadhani',
+                   'nim'                : '231031037',
+                   'prodi'              : 'Sistem Informasi',
+                   'hobi'               : 'Memasak',
+                   'tanggal_lahir'      : '18 oktober 2004',
+                   'tempat_lahir'       : 'bulu tellue',
+                   'kelas'              : 'SI-23B',
+                   'kampus'             : 'Institut Teknologi BJ Habibie',
+}
+                   
+print()
+print('Nama  :',biodata['nama'])
+print('Nim   :',biodata['nim'])
+print('kelas :',biodata['kelas'])
+print('Prodi :',biodata['prodi'])
+print('\n')
+print('Perkenalkan nama saya',biodata['nama'],'dari program studi',biodata['prodi'],'dengan Nim',biodata['nim'],'nama kampus saya adalah',biodata['kampus'],'tempat tanggal lahir saya',biodata['tempat_lahir'],biodata['tanggal_lahir'],'hobi saya adalah',biodata['hobi'])
+                   
