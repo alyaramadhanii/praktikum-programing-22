de343028250cce68ac913539c497924479cb9f93
alyaramadhanii <153715494+alyaramadhanii@users.noreply.github.com>
2023-12-19 21:19:15 +0800

    Add files via upload

---
+++ tugas- percabangan.py
@@ -0,0 +1,13 @@
+print('Nama Lengkap: Alya Ramadhani')
+print('NIM         : 231031037')
+print('prodi       : Sistem Informasi')
+print()
+
+angka = int(input("masukkan nilai :"))
+if angka % 2 != 0:
+    print("bilangan x adalah ganjil")
+else:
+    print("bilangan x adalah bukan ganjil")
+print()
+print(f"tugas 1: buat program")
+print(f"tugas 1: program penjumlahan waktu")
